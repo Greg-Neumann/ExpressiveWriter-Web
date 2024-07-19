1129bf867ba9afe9db920771ab8794282842f2ba
Greg Neumann on his iMac <GregInTheNorth@icloud.com>
2024-07-19 14:38:52 +0100

    WIP

---
+++ src/components/PlaceHolder.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-card
+    class="my-card text-white"
+    style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+  >
+    <q-card-section>
+      <div class="text-h6">Expressive Writer</div>
+      <div class="text-subtitle2">by Greg Neumann</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none"> This is sample text </q-card-section>
+  </q-card>
+</template>
+
+<script setup lang="ts"></script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,106 +1,35 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh lpR fFf">
+    <q-header reveal class="bg-primary text-white">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title> Expressive Writer </q-toolbar-title>
+        <q-btn-group push glossy>
+          <q-btn
+            color="grey"
+            text-color="black"
+            label="Timeline"
+            icon="timeline"
+          />
+          <q-btn color="grey" text-color="black" label="List" icon="list" />
+          <q-btn
+            color="grey"
+            text-color="black"
+            label="Calendar"
+            icon="calendar_month"
+          />
+        </q-btn-group>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
+      <div class="q-pa-md q-gutter-y-md column items-start"></div>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
-
 defineOptions({
-  name: 'MainLayout'
+  name: 'MainLayout',
 });
-
-const linksList: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
-const leftDrawerOpen = ref(false);
-
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-}
 </script>
